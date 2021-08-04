7f0f077c25d2ed6985f44d9abf6bb156a503333b
Qing Fu <zz5840@qq.com>
2021-08-04 19:22:56 +0800

    fix: 🐛 Fix declaration export error

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,3 @@
 import { IMap } from "@fannithm/const/js/pjsk";
 
-declare module '@fannithm/sus-fannihtm-convertor' {
-    function convertor(sus: string): IMap;
-    export = convertor;
-}
+export default function convertor(sus: string): IMap;
